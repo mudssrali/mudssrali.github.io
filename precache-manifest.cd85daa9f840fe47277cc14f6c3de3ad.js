843d2abdf05eb2994253cf975b099ef850534db6
mudassar045 <mudassaralics007@gmail.com>
2020-11-30 23:25:32 +0500

    New version with feedback form

---
+++ precache-manifest.cd85daa9f840fe47277cc14f6c3de3ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11c06329d085876edf7b29b42563a6c",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "245a94b95fd1b4957f9e",
+    "url": "/blog/static/css/main.6d92174a.chunk.css"
+  },
+  {
+    "revision": "e3330023fb9cadfab176",
+    "url": "/blog/static/js/2.62da6641.chunk.js"
+  },
+  {
+    "revision": "7639adc58ffd78f42cdcfed17560032f",
+    "url": "/blog/static/js/2.62da6641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "245a94b95fd1b4957f9e",
+    "url": "/blog/static/js/main.55e8649b.chunk.js"
+  },
+  {
+    "revision": "80640c4e9485211ab9f4",
+    "url": "/blog/static/js/runtime-main.6d791957.js"
+  },
+  {
+    "revision": "f64a5e4e42d58b1cdfae87863dc23f9b",
+    "url": "/blog/static/media/mudassar-ali.f64a5e4e.jpg"
+  }
+]);
